73350b30fa5368120301b3fe2154674da9e5b767
Brad Wilson <brad@rhift.com>
2018-12-10 23:22:18 -0600

    Update BradfabInterface methods

--- src/BradfabInterface.php
+++ src/BradfabInterface.php
@@ -3,8 +3,26 @@ declare(strict_types=1);
 
 namespace Rhift\Bradfab;
 
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
+
 interface BradfabInterface
 {
-
     public function fabricate();
+
+    public function setFilesystem(Filesystem $filesystem): BradfabInterface;
+
+    public function setFinder(Finder $finder): BradfabInterface;
+
+    public function setTargetNamespace(string $target_namespace): BradfabInterface;
+
+    public function getFilesystem(): Filesystem;
+
+    public function getFabricationPath(): string;
+
+    public function setSourcePath(string $source_path): BradfabInterface;
+
+    public function setFabricationPath(string $fabrication_path): BradfabInterface;
+
+    public function getTargetNamespace(): string;
 }
