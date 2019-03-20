35e46e52eabca5dcfa97224aef667ffe61cd23fc
jacobmalachowski <jacob.malachowski@55places.com>
2019-03-20 13:49:03 -0500

    add handler template

---
+++ src/Template/Actor/Repository/HandlerInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Neighborhoods\Bradfab\Template\Actor\Repository;
+
+interface HandlerInterface extends \Psr\Http\Server\RequestHandlerInterface
+{
+
+    const ROUTE_PATH_ActorS = '';
+
+    const ROUTE_NAME_ActorS = '';
+
+
+}
+
