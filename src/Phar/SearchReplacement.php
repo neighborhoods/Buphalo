59534d4d092c24a15da26729e42ee7667fef13b7
Jacques Marcotte <jacques.marcotte@neighborhoods.com>
2019-09-25 23:40:19 -0500

    BUPH-65 | cleanup phar builder

---
+++ src/Phar/SearchReplacement.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Neighborhoods\Buphalo\Phar;
+
+class SearchReplacement implements SearchReplacementInterface
+{
+    /** @var string */
+    private $search;
+
+    /** @var string */
+    private $replacement;
+
+    public function getSearch(): string
+    {
+        if ($this->search === null) {
+            throw new \LogicException('Replacement pattern has not been set.');
+        }
+
+        return $this->search;
+    }
+
+    public function setSearch(string $search): SearchReplacementInterface
+    {
+        if ($this->search !== null) {
+            throw new \LogicException('Replacement pattern is already set.');
+        }
+
+        $this->search = $search;
+
+        return $this;
+    }
+
+    public function getReplacement(): string
+    {
+        if ($this->replacement === null) {
+            throw new \LogicException('Replacement replacement has not been set.');
+        }
+
+        return $this->replacement;
+    }
+
+    public function setReplacement(string $replacement): SearchReplacementInterface
+    {
+        if ($this->replacement !== null) {
+            throw new \LogicException('Replacement replacement is already set.');
+        }
+
+        $this->replacement = $replacement;
+
+        return $this;
+    }
+}
