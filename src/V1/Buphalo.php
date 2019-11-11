3c712c1d9514f6112d591e26dfd6dec410041475
Brad Wilson <brad@rhift.com>
2019-11-11 07:52:13 -0800

    Can cache container as a runtime option.

--- src/V1/Buphalo.php
+++ src/V1/Buphalo.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace Neighborhoods\Buphalo\V1;
 
+use InvalidArgumentException;
+use LogicException;
 use Neighborhoods\Buphalo\V1\Fabricator\FactoryInterface;
 use Neighborhoods\Buphalo\V1\Protean;
 use ReflectionClass;
@@ -11,8 +13,14 @@ class Buphalo implements BuphaloInterface
 {
     use Protean\Container\Builder\AwareTrait;
 
+    protected const CAN_CACHE_CONTAINER = 'Neighborhoods_Buphalo_V1_Buphalo__CanCacheContainer';
+
+    private $CanCacheContainerFromEnvironment;
+    private $CanCacheContainer;
+
     public function run(): BuphaloInterface
     {
+        /** @noinspection PhpUnhandledExceptionInspection */
         $this->getProteanContainerBuilder()->setCachedContainerFileName(
             sprintf(
                 '%s.php',
@@ -20,11 +28,51 @@ class Buphalo implements BuphaloInterface
             )
         );
         $this->getProteanContainerBuilder()->setCanBuildZendExpressive(false);
-        $this->getProteanContainerBuilder()->setCanCacheContainer(false);
+        $this->getProteanContainerBuilder()->setCanCacheContainer($this->getCanCacheContainer());
         $this->getProteanContainerBuilder()->registerServiceAsPublic(FactoryInterface::class);
         $fabricator = $this->getProteanContainerBuilder()->build()->get(FactoryInterface::class)->create();
         $fabricator->fabricate();
 
         return $this;
+    }
+
+    public function getCanCacheContainer(): bool
+    {
+        if ($this->CanCacheContainer === null) {
+            $this->CanCacheContainer = $this->getCanCacheContainerFromEnvironment();
+        }
+
+        return $this->CanCacheContainer;
+    }
+
+    public function setCanCacheContainer(bool $CanCacheContainer): BuphaloInterface
+    {
+        if ($this->CanCacheContainer !== null) {
+            throw new LogicException('Can Cache Container is already set.');
+        }
+
+        $this->CanCacheContainer = $CanCacheContainer;
+
+        return $this;
+    }
+
+    protected function getCanCacheContainerFromEnvironment(): bool
+    {
+        if (isset($_ENV[self::CAN_CACHE_CONTAINER])) {
+            if ($_ENV[self::CAN_CACHE_CONTAINER] === 'true' || $_ENV[self::CAN_CACHE_CONTAINER] === 'false') {
+                $this->CanCacheContainerFromEnvironment = (bool)$_ENV[self::CAN_CACHE_CONTAINER];
+            } else {
+                throw new InvalidArgumentException(
+                    sprintf('%s is not boolean. Given "%s"',
+                        self::CAN_CACHE_CONTAINER,
+                        $_ENV[self::CAN_CACHE_CONTAINER]
+                    )
+                );
+            }
+        } else {
+            $this->CanCacheContainerFromEnvironment = false;
+        }
+
+        return $this->CanCacheContainerFromEnvironment;
     }
 }
