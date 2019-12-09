d39d0f32199b31855b9362942ae2829e8f5737fc
Jacques Marcotte <jacques.marcotte@neighborhoods.com>
2019-12-09 10:41:45 -0600

    BUPH-21 | add test for multiple annotations in a file

---
+++ tests/v1/MultipleAPs/control/MultipleAnnotations.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace Neighborhoods\BuphaloTest;
+
+class MultipleAnnotations
+{
+    public const TEST_STRING = '';
+
+    public const SECOND = 'second';
+}
