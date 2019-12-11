3214818d42fe008e11c6f7b7ddd73317e7eef6e2
Jacques Marcotte <jacques.marcotte@neighborhoods.com>
2019-12-11 15:28:48 -0600

    HOTFIX | add V1 to phar self-buphalo script

--- bin/v1/buphalo-buphalo-from-phar.sh
+++ bin/v1/buphalo-buphalo-from-phar.sh
@@ -6,5 +6,5 @@ cd ../..
 Neighborhoods_Buphalo_V1_TargetApplication_BuilderInterface__SourceDirectoryPath=$PWD/src \
 Neighborhoods_Buphalo_V1_TargetApplication_BuilderInterface__FabricationDirectoryPath=$PWD/fab \
 Neighborhoods_Buphalo_V1_TargetApplication_BuilderInterface__NamespacePrefix=Neighborhoods\\Buphalo\\ \
-Neighborhoods_Buphalo_V1_TemplateTree_Map_Builder_FactoryInterface__TemplateTreeDirectoryPaths=$PWD/template-tree,$PWD/template-tree1 \
+Neighborhoods_Buphalo_V1_TemplateTree_Map_Builder_FactoryInterface__TemplateTreeDirectoryPaths=$PWD/template-tree/V1,$PWD/template-tree1 \
 php bin/v1/buphalo.phar
