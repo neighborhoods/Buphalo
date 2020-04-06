6a72fef3aaa269b18751115b4a591482f979dff9
Jacques Marcotte <jacques.marcotte@neighborhoods.com>
2020-04-06 17:20:36 -0500

    BUPH-116 | add Duplicate and Repeated AP tests

---
+++ tests/v1/RepeatedAnnotationProcessor/templates/PrimaryActorName.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace Neighborhoods\BuphaloTemplateTree;
+
+class PrimaryActorName
+{
+    public const TEST_STRING = "/** @neighborhoods-buphalo:annotation-processor TestSimpleStringAnnotationProcessor
+    */";
+
+    public const ANOTHER_STRING = "/** @neighborhoods-buphalo:annotation-processor DifferentSimpleStringAnnotationProcessor
+    remove me
+    */";
+}
