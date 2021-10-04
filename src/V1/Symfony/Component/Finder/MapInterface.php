48cf6b772cfe486e4ad4c9d53182dc91d8f995b5
Jacques Marcotte <jacques.marcotte@neighborhoods.com>
2021-10-04 13:32:50 -0500

    HOTFIX | update finder maps offsetSet to return void

--- src/V1/Symfony/Component/Finder/MapInterface.php
+++ src/V1/Symfony/Component/Finder/MapInterface.php
@@ -18,7 +18,7 @@ interface MapInterface extends SeekableIterator, ArrayAccess, Serializable, Coun
     public function offsetGet($index): Finder;
 
     /** @param Finder $finder */
-    public function offsetSet($index, $finder);
+    public function offsetSet($index, $finder): void;
 
     /** @param Finder $finder */
     public function append($finder);
