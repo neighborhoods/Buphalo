9fc94ccad5641e43421abc277d8ef39cf948a572
Jacques Marcotte <jacques.marcotte@neighborhoods.com>
2021-11-17 18:49:55 -0600

    BUPH-141 | remove v2 tests in prep for v2 tokenization/compilation

--- tests/v2/GlobalTemplateTrees/control/GlobalTreeTestInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace Neighborhoods\BuphaloTest;
-
-interface GlobalTreeTestInterface
-{
-    // This is in the secondary tree
-}
--- tests/v2/GlobalTemplateTrees/templates/primary/PrimaryActorName.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace Neighborhoods\BuphaloTemplateTree;
-
-class PrimaryActorName implements PrimaryActorNameInterface
-{
-    // This is in the primary tree
-}
